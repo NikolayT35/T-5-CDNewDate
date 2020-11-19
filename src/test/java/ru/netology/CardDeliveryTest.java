d3e316022186e559b895cfce7e2c46a90c8d1690
Nikoly <turnik3580@gmail.com>
2020-11-19 22:07:05 +0300

    edit project

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -11,30 +11,30 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-    DataGenerator dataGenerator = new DataGenerator();
 
-    String name = dataGenerator.getName();
-    String phone = dataGenerator.getPhone();
-    String city = dataGenerator.getCity();
+
+    String name = DataGenerator.getName();
+    String phone = DataGenerator.getPhone();
+    String city = DataGenerator.getCity();
 
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(3));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.createDate(3));
         form.$("[data-test-id=name] input").setValue(name);
         form.$("[data-test-id=phone] input").setValue(phone);
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".notification_status_ok").shouldBe(exist);
-        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на  " + dataGenerator.createDate(3)));
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(7));
+        $(".notification_status_ok").shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на  " + DataGenerator.createDate(3)));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.createDate(7));
         form.$(".button").click();
-        $("[data-test-id=replan-notification]").waitUntil(exist, 15000);
+        $("[data-test-id=replan-notification]").waitUntil(visible, 15000);
         $(withText("Перепланировать")).click();
-        $(".notification_status_ok").shouldBe(exist);
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на  " + dataGenerator.createDate(7)));
+        $(".notification_status_ok").shouldBe(visible);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на  " + DataGenerator.createDate(7)));
     }
 }
 
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import com.github.javafaker.Faker;
 
 
@@ -6,30 +7,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public class DataGenerator {
+ public class DataGenerator {
+   private DataGenerator() {
+    }
 
 
-private Faker faker;
-    LocalDate today = LocalDate.now();
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static Faker faker;
+    private static LocalDate today = LocalDate.now();
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String createDate(int plusDays) {
+    public static String createDate(int plusDays) {
         LocalDate newDate = today.plusDays(plusDays);
         return formatter.format(newDate);
     }
 
-    public String getCity() {
+    public static String getCity() {
         String[] CityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
         int city = (int) Math.floor(Math.random() * CityList.length);
         return CityList[city];
     }
-    public String getName() {
-       faker = new Faker(new Locale("ru"));
-        return (faker.name().lastName()+(" ")+faker.name().firstName());
+
+    public static String getName() {
+        faker = new Faker(new Locale("ru"));
+        return (faker.name().lastName() + (" ") + faker.name().firstName());
     }
 
-    public String getPhone() {
-      faker = new Faker(new Locale("ru"));
+    public static String getPhone() {
+        faker = new Faker(new Locale("ru"));
         return faker.phoneNumber().phoneNumber();
     }
 }
